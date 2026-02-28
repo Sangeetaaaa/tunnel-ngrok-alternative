eff6eb2624546c87461fce855d8fc198bd09a53d
Sangeetaaaa <sangeeta3112002@gmail.com>
2026-02-28 17:25:42 +0530

    add dynamic port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { WebSocketServer } = require("ws");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json()); // Parse JSON bodies
 
